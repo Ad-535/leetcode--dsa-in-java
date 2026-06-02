19dcb87d8c0a1f3019042dbc3e6901a3a9f8a25a
Adarsh Gupta <adarsh7711gupta@gmail.com>
2026-06-02 13:21:54 +0530

    Time: 1 ms (99.97%), Space: 47 MB (44.78%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,8 +1,8 @@
 class Solution {
     public int singleNumber(int[] nums) {
         int sum = 0;
-        for(int arr:nums){
-            sum^=arr;
+        for(int i=0;i<nums.length;i++){
+            sum^=nums[i];
         }
         return sum;
     }
