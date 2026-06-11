d72b88a3f742d055203ef7c773819625694094f3
Adarsh Gupta <adarsh7711gupta@gmail.com>
2026-06-11 13:17:24 +0530

    Time: 31 ms (11.81%), Space: 62.2 MB (5%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -1,13 +1,13 @@
 class Solution {
     public int longestCommonSubsequence(String text1, String text2) {
-        int dp[][] = new int[text1.length()][text2.length()];
-        for(int r[]:dp)Arrays.fill(r,-2);
+        Integer dp[][] = new Integer[text1.length()][text2.length()];
+        
        return lcs(text1,text2,0,0,dp);
     }
-     int lcs(String s1, String s2, int i,int j,int dp[][]){
+     int lcs(String s1, String s2, int i,int j,Integer dp[][]){
       if(i==s1.length() || j==s2.length())
       return 0;
-      if(dp[i][j]!=-2) return dp[i][j];
+      if(dp[i][j]!=null) return dp[i][j];
       if(s1.charAt(i)==s2.charAt(j)){
         return dp[i][j] = 1 + lcs(s1,s2,i+1,j+1,dp);
       }
