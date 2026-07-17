38c80f3689e4a2b296a0fc702de7e66f7ac52f0e
Adarsh Gupta <adarsh7711gupta@gmail.com>
2026-07-18 00:24:12 +0530

    Time: 29 ms (87.5%), Space: 99.8 MB (68.75%) - LeetHub

--- 3312-sorted-gcd-pair-queries/3312-sorted-gcd-pair-queries.java
+++ 3312-sorted-gcd-pair-queries/3312-sorted-gcd-pair-queries.java
@@ -1,54 +1,45 @@
 class Solution {
+
     public int[] gcdValues(int[] nums, long[] queries) {
-
-        int max = 0;
-        for (int x : nums)
-            max = Math.max(max, x);
-
-        int[] freq = new int[max + 1];
-
-        for (int x : nums)
-            freq[x]++;
-
-        long[] gcdCount = new long[max + 1];
-
-        for (int g = max; g >= 1; g--) {
-
-            long cnt = 0;
-
-            for (int multiple = g; multiple <= max; multiple += g) {
-                cnt += freq[multiple];
-                gcdCount[g] -= gcdCount[multiple];
+        int m = 0;
+        for (int num : nums) {
+            m = Math.max(m, num);
+        }
+        long[] cnt = new long[m + 1];
+        for (int num : nums) {
+            cnt[num]++;
+        }
+        for (int i = 1; i <= m; i++) {
+            for (int j = i * 2; j <= m; j += i) {
+                cnt[i] += cnt[j];
             }
-
-            gcdCount[g] += cnt * (cnt - 1) / 2;
         }
-
-        for (int i = 2; i <= max; i++)
-            gcdCount[i] += gcdCount[i - 1];
-
+        for (int i = 1; i <= m; i++) {
+            cnt[i] = (cnt[i] * (cnt[i] - 1)) / 2;
+        }
+        for (int i = m; i >= 1; i--) {
+            for (int j = i * 2; j <= m; j += i) {
+                cnt[i] -= cnt[j];
+            }
+        }
+        for (int i = 1; i <= m; i++) {
+            cnt[i] += cnt[i - 1];
+        }
         int[] ans = new int[queries.length];
-
-        for (int i = 0; i < queries.length; i++) {
-            ans[i] = upperBound(gcdCount, queries[i]);
+        for (int k = 0; k < queries.length; k++) {
+            long q = queries[k] + 1;
+            int left = 1,
+                right = m;
+            while (left < right) {
+                int mid = (left + right) / 2;
+                if (cnt[mid] >= q) {
+                    right = mid;
+                } else {
+                    left = mid + 1;
+                }
+            }
+            ans[k] = left;
         }
-
         return ans;
-    }
-
-    private int upperBound(long[] arr, long target) {
-        int left = 1;
-        int right = arr.length - 1;
-
-        while (left < right) {
-            int mid = left + (right - left) / 2;
-
-            if (arr[mid] > target)
-                right = mid;
-            else
-                left = mid + 1;
-        }
-
-        return left;
     }
 }
