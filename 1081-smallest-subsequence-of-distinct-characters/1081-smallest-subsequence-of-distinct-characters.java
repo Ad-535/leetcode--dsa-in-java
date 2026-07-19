88151d289051e14e01e0ff15d56d03334fb3fcc3
Adarsh Gupta <adarsh7711gupta@gmail.com>
2026-07-19 08:21:23 +0530

    Time: 11 ms (6.08%), Space: 44.9 MB (8.14%) - LeetHub

---
+++ 1081-smallest-subsequence-of-distinct-characters/1081-smallest-subsequence-of-distinct-characters.java
@@ -0,0 +1,29 @@
+class Solution {
+    public String smallestSubsequence(String s) {
+        if (s.length() == 0) return "";
+
+        int[] count = new int[26];
+
+        for (char c : s.toCharArray()) {
+            count[c - 'a']++;
+        }
+
+        int pos = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) < s.charAt(pos))
+                pos = i;
+
+            count[s.charAt(i) - 'a']--;
+
+            if (count[s.charAt(i) - 'a'] == 0)
+                break;
+        }
+
+        char first = s.charAt(pos);
+
+        String remaining = s.substring(pos + 1).replaceAll("" + first, "");
+
+        return first + smallestSubsequence(remaining);
+    }
+}
