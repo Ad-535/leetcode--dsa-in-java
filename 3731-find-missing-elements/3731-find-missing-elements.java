78128dbd43681d4eab16e1a15fc552a190d886d2
Adarsh Gupta <adarsh7711gupta@gmail.com>
2026-08-04 06:45:28 +0530

    Time: 9 ms (6.99%), Space: 47.3 MB (18.67%) - LeetHub

---
+++ 3731-find-missing-elements/3731-find-missing-elements.java
@@ -0,0 +1,16 @@
+class Solution {
+    public List<Integer> findMissingElements(int[] nums) {
+        Arrays.sort(nums);
+        int n = nums[nums.length-1];
+        List<Integer> ans = new ArrayList<>();
+        TreeSet<Integer> set = new TreeSet<>();
+        for(int i:nums){
+            set.add(i);
+        }
+        for(int i=nums[0];i<=n;i++){
+            if(!set.contains(i))
+            ans.add(i);
+        }
+        return ans;
+    }
+}
