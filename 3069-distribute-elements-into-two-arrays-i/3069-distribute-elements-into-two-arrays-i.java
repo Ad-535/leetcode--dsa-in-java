10c32ec01f840e997a2d12fc283b9c68bde7c8d6
Adarsh Gupta <adarsh7711gupta@gmail.com>
2026-08-20 22:32:19 +0530

    Time: 1 ms (98.08%), Space: 46.5 MB (90.87%) - LeetHub

--- 3069-distribute-elements-into-two-arrays-i/3069-distribute-elements-into-two-arrays-i.java
+++ 3069-distribute-elements-into-two-arrays-i/3069-distribute-elements-into-two-arrays-i.java
@@ -2,25 +2,25 @@ class Solution {
     public int[] resultArray(int[] nums) {
         int arr1[] = new int[nums.length];
         int arr2[] = new int[nums.length];
-        int i = 0;
-        int j = 0;
-        arr1[i] = nums[0];
-        arr2[j] = nums[1];
+        int i=0;
+        int j=0;
+        arr1[i]=nums[0];
+        arr2[j]=nums[1];
         for(int k=2;k<nums.length;k++){
             if(arr1[i]>arr2[j]){
-                arr1[++i] = nums[k];
+                arr1[++i]=nums[k];
             }
             else{
-                arr2[++j] = nums[k];
+                arr2[++j]=nums[k];
             }
         }
-        int ans[] = new int[nums.length];
-        for(int k=0;k<=i;k++){
-            ans[k] = arr1[k];
-        }
-        for(int k=0;k<=j;k++){
-            ans[k+1+i] = arr2[k];
-        }
+            int ans[] = new int[nums.length];
+            for(int k=0;k<=i;k++){
+                ans[k] = arr1[k];
+            }
+            for(int k=0;k<=j;k++){
+                ans[i+k+1]=arr2[k];
+            }
         return ans;
     }
 }
