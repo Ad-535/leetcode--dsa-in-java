e23824e269e983ad87a18371ff486d5a281413e4
Adarsh Gupta <adarsh7711gupta@gmail.com>
2026-09-06 01:18:17 +0530

    Time: 0 ms (100%), Space: 42.8 MB (79.84%) - LeetHub

---
+++ 0054-spiral-matrix/0054-spiral-matrix.java
@@ -0,0 +1,34 @@
+class Solution {
+    public List<Integer> spiralOrder(int[][] arr) {
+        List<Integer> ans = new ArrayList<>();
+        int minr = 0;
+        int minc = 0;
+        int maxr = arr.length-1;
+        int maxc = arr[0].length-1;
+        int c = 0;
+        int t = arr.length*arr[0].length;
+        while(c<t){
+        for(int i=minc;i<=maxc && c<t;i++){
+            ans.add(arr[minr][i]);
+            c++;
+        }
+        minr++;
+        for(int i=minr;i<=maxr && c<t;i++){
+            ans.add(arr[i][maxc]);
+            c++;
+        }
+        maxc--;
+        for(int i=maxc;i>=minc && c<t;i--){
+            ans.add(arr[maxr][i]);
+            c++;
+        }
+        maxr--;
+        for(int i=maxr;i>=minr && c<t;i--){
+            ans.add(arr[i][minc]);
+            c++;
+        }
+        minc++;
+        }
+        return ans;
+    }
+}
